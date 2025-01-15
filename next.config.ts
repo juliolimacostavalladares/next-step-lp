c86b770283ba8ac66668505421db0d15d7d7bd61
Julio Lima Costa Valladares <juliolima1225@gmail.com>
2025-01-15 08:51:43 -0300

    add config to gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: isProd ? '/next-step-lp/' : '',
   images: {
     unoptimized: true,
   },
